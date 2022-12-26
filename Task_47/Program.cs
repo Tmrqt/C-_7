3eb8ad9d0c18acc1fe2e6f4e20fa5097c0abd90b
Timerkhan <timerkhan12@gmail.com>
2022-12-27 02:30:52 +0500

    done

---
+++ Task_47/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите кол-во строк массив m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов массив n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NewArr (int m, int n)
+{
+    double[,] arr = new double[m,n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            arr[i,j] = Convert.ToDouble(rand.Next(-100, 100)) / 10;
+            System.Console.Write(arr[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+NewArr(m,n);
